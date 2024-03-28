818aba6cf4a77381bc47fc92c738838fde6d236e
Jerry <2963025245@qq.com>
2024-03-28 14:54:08 +0800

    主题框架完成

--- Tetris.cpp
+++ Tetris.cpp
@@ -1,1 +1,67 @@
 #include "Tetris.h"
+
+Tetris::Tetris(int rows, int cols, int left, int top, int blockSize)
+{
+}
+
+void Tetris::init()
+{
+	delay = 30;
+}
+
+void Tetris::play()
+{
+	init();
+	int timer = 0;
+	while (1){
+	/*	//首先接受用户输入
+		keyEven();
+		//再渲染游戏画面
+		updateWindow();
+		这种每输入就一次渲染对cpu负荷较大
+		使用另一种方法
+	*/
+		keyEven();
+		timer += getDelay();
+		//keyEven函数只在play中使用，就设为私有;
+		if (timer > delay) {
+
+			//渲染游戏画面
+			drop();
+			timer = 0;
+			update = true;
+		}
+		if (update)
+		{
+			update = false;
+            updateWindow();
+		}
+		
+
+	}
+}
+
+void Tetris::keyEven()
+{
+	update = true;
+	//to do.开始做
+}
+
+void Tetris::updateWindow()
+{
+
+}
+
+int Tetris::getDelay()
+{
+
+	return 0;
+}
+
+void Tetris::drop()
+{
+}
+
+void Tetris::clearLine()
+{
+}
--- Tetris.h
+++ Tetris.h
@@ -5,7 +5,17 @@ public:
 	Tetris(int rows,int cols,int left,int top,int blockSize );
 	void init();
 	void play();
-
-
+private:
+	void keyEven();//不适合公开，因为是play的内部使用的，使用private
+	void updateWindow();
+	//第一次调用函数，返回0
+	int getDelay();//返回距离上一次调用该函数，间隔了多少时间
+	void drop();
+	void clearLine();
+  
+private:
+	//私有数据成员
+	int delay;
+	bool update;//是否更新
 };
 
